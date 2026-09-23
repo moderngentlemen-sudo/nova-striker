886d87e68ee9f82b66a6ca9406048437a783fae5
moderngentlemen-sudo <welcome@moderngentlemen.co>
2026-09-23 05:37:06 -0500

    feat: add Style rank gameplay cue

--- UnityProject/Assets/Scripts/Core/GameplayEventHub.cs
+++ UnityProject/Assets/Scripts/Core/GameplayEventHub.cs
@@ -37,6 +37,7 @@ namespace NovaStriker.Core
         GuardianAbilityResolved,
         TeamSyncStarted,
         TeamSyncResolved,
+        StyleRankChanged,
         CampaignCompleted,
         ParryStarted,
         ParrySuccess,
