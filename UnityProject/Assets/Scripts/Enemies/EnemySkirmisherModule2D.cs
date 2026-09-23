091ecc755077a5b9ceca246e9684c936dfa632fb
moderngentlemen-sudo <welcome@moderngentlemen.co>
2026-09-23 10:31:29 -0500

    Use projectile pool for runtime shots

--- UnityProject/Assets/Scripts/Enemies/EnemySkirmisherModule2D.cs
+++ UnityProject/Assets/Scripts/Enemies/EnemySkirmisherModule2D.cs
@@ -95,7 +95,7 @@ namespace NovaStriker.Enemies
             Vector2 direction =
                 ((Vector2)brain.Target.position - (Vector2)origin).normalized;
 
-            Projectile2D projectile = Instantiate(
+            Projectile2D projectile = ProjectilePool2D.Spawn(
                 projectilePrefab,
                 origin,
                 Quaternion.identity
